2bab838a9c3478b4154c709fbf0b12dc3e40a008
陈海洋 <1559843332@qq.com>
2020-04-17 10:18:50 +0800

    提供get set方法

--- README.md
+++ README.md
@@ -8,7 +8,60 @@
 # 使用方式
 
 ### 1.引入依赖
-```$xslt
+```xml
+<dependency>
+    <groupId>cn.chendahai</groupId>
+    <artifactId>dingding-spring-boot-starter</artifactId>
+    <version>2.0.1</version>
+</dependency>
+```
+
+### 2. 注入bean
+> 直接在application启动类添加bean注解方式注入
+```java
+@Bean
+public DingdingService dingdingService() {
+    DingdingService dingdingService = new DingdingService();
+    // 设置全局的手机号或者token，可选
+    // dingdingService.setPhone("13121939122");
+    // dingdingService.setToken("dingdingtokne");
+    return dingdingService;
+}
+```
+
+### 3. @Autowired注入使用
+
+> 注入service
+```java
+@Autowired
+DingdingService dingdingService;
+
+// 2. 发送消息
+dingdingService.send("msg");
+
+```
+
+### 4. 调用API发送消息
+```java
+send(String content) 
+sendAll(String content) 
+sendAllByToken(String content, String token) 
+sendByPhone(String content, String phone) 
+sendByToken(String content, String token) 
+sendByTokenAndPhone(String content, String token, String phone) 
+```
+> 方法和说明
+- content 钉钉消息发送的内容
+- token 申请的钉钉token
+- phone 发送消息需要@的用户，多个手机号用逗号分隔
+- all 表示@所有人
+
+---
+
+# 自用方式（可忽略）
+
+### 1.引入依赖
+```xml
 <dependency>
     <groupId>cn.chendahai</groupId>
     <artifactId>dingding-spring-boot-starter</artifactId>
@@ -17,7 +70,7 @@
 ```
 
 ### 2. 配置application配置文件
-```$xslt
+```yaml
 dingding:
   service:
 # 开启钉钉支持
@@ -29,7 +82,7 @@ dingding:
 ```
 
 ### 3. @Autowired注入使用
-```$xslt
+```java
 @Autowired
 DingdingService dingdingService;
 
--- src/main/java/cn/chendahai/dingding/service/DingdingService.java
+++ src/main/java/cn/chendahai/dingding/service/DingdingService.java
@@ -20,62 +20,66 @@ public class DingdingService {
         this.phone = phone;
     }
 
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
     /**
      * @param content Content information sent
-     * @return
      */
     public Map<String, Object> send(String content) {
         return DingDingSendMsg.sendText(content, token, phone, false);
     }
 
     /**
-     *
      * @param content Content information sent
      * @param phone Mobile phone number, multiple separated by a comma
-     * @return
      */
     public Map<String, Object> sendByPhone(String content, String phone) {
         return DingDingSendMsg.sendText(content, token, phone, false);
     }
 
     /**
-     *
      * @param content Content information sent
      * @param token dingding token
-     * @return
      */
     public Map<String, Object> sendByToken(String content, String token) {
         return DingDingSendMsg.sendText(content, token, phone, false);
     }
 
     /**
-     *
      * @param content Content information sent
      * @param token dingding token
      * @param phone Mobile phone number, multiple separated by a comma
-     * @return
      */
     public Map<String, Object> sendByTokenAndPhone(String content, String token, String phone) {
         return DingDingSendMsg.sendText(content, token, phone, false);
     }
 
     /**
-     *
      * @param content Content information sent
-     * @return
      */
     public Map<String, Object> sendAll(String content) {
         return DingDingSendMsg.sendText(content, token, phone, true);
     }
 
     /**
-     *
      * @param content Content information sent
      * @param token dingding token
-     * @return
      */
     public Map<String, Object> sendAllByToken(String content, String token) {
         return DingDingSendMsg.sendText(content, token, phone, true);
     }
-
 }
